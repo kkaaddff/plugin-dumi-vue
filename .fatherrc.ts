fef6197eb1cac06d33be03b10e16b19c18bfa120
章启成 <910021538@qq.com>
2021-12-13 19:06:48 +0800

    feat(build): father build config

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,12 +1,7 @@
-export default [{
-  target: 'node',
-  cjs: { type: 'babel', lazy: true },
-  disableTypeCheck: true,
-  extraBabelPlugins: [
-    [
-      'babel-plugin-import',
-      { libraryName: 'antd', libraryDirectory: 'es', style: true },
-      'antd',
-    ],
-  ],
-}];
+export default [
+  {
+    target: 'node',
+    cjs: { type: 'babel', lazy: true },
+    disableTypeCheck: true,
+  },
+];
